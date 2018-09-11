d985b550cecc8e9b1a49cc45c265f3cee45a844f
Xianxiangdaishu <1546440123@qq.com>
2018-09-11 09:23:59 +0800

    Mm

---
+++ www/orm.py
@@ -0,0 +1,17 @@
+import asyncio, logging
+
+import aiomysql
+
+def log(sql,args=()):
+    logging.info('SQL: %s' %sql)
+
+async def create_pool(loop,**kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host','localhost'),
+        port=kw.get('port',3306),
+        user=kw['user'],
+
+
+    )
